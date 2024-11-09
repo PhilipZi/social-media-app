8f35477e7ef24a3c50b80b9edf66484c260a76cc
Philip Zimmermann <pzi@ito-consult.de>
2024-11-09 19:54:55 +0100

    add home icon with text via slot and active prop

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,25 @@
+<template>
+    <NuxtLink to="/" class="flex items-center p-3 w-min hover:bg-gray-200 rounded-full" :class="defaultTransition">
+        <div class="w-6 h-6 text-dark" >
+            <slot name="icon" />
+        </div>
+        <div class="ml-4 text-xl" :class="textClasses">
+            <slot name="name" />
+        </div>
+    </NuxtLink>
+</template>
+<script setup>
+const {defaultTransition} = useTailwindConfig();
+
+const props = defineProps({
+active: {
+    type: Boolean,
+    default: false,
+}
+});
+
+const textClasses = computed(() => {
+    return props.active ? 'font-semibold' : 'font-normal';
+});
+
+</script>
---
+++ components/Sidebar/Left/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="h-screen flex flex-col ">
+    <!-- HomeIcon -->
+    <div class="p-2 my-2 rounded-full hover:bg-blue-50 w-min
+    dark:hover:bg-white/20" :class="defaultTransition">
+        <NuxtLink to="/" class="flex items-center space-x-2">
+          <div class="w-8 h-8">
+            <LogoTwitter />
+          </div>
+        </NuxtLink>
+    </div>
+    <SidebarLeftTab active>
+      <template v-slot:icon>
+      <HomeIcon  />
+      </template>
+      <template v-slot:name>
+        Home
+      </template>
+    </SidebarLeftTab>
+
+  </div>
+</template>
+
+<script setup>
+import {HomeIcon} from '@heroicons/vue/24/solid';
+const {defaultTransition} = useTailwindConfig();
+</script>
+
